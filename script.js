24ecdc53e85f43e8324740a25f58ad72a8be3b7a
Pawel Kurek <pawel.kurek975@gmail.com>
2021-11-10 01:11:58 +0100

    summary initialize

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const bill = document.querySelector(".input-bill");
 const selectTip = document.querySelectorAll(".btn-tip")
 const inputTip = document.querySelector(".custom-input")
 const inputNumberOfPeople = document.querySelector(".input-number-of-people")
+const tipAmount = document.querySelector(".tip-amount")
+const totalTip = document.querySelector(".total-tip")
 
 let billState;
 let tipPercent;
 let numberOfPeople;
 
+function updateState() {
+   tipAmount.textContent = (billState * tipPercent)/100 
+    totalTip.textContent = (billState * tipPercent)/100  * numberOfPeople
+}
+
 bill.addEventListener("input", () => {
     billState = bill.value
+    updateState()
 })
 
 selectTip.forEach(tip => {
@@ -17,22 +25,22 @@ selectTip.forEach(tip => {
         e.target.setAttribute("aria-selected", "true")
         let tipValue = e.target.textContent
         tipPercent = Number(tipValue.substring(0,tipValue.length-1))
-        // console.log(billState, tipPercent, numberOfPeople)
+        updateState()
     })
 })
 
 inputTip.addEventListener("input", (e)=>{
     selectTip.forEach(tip => tip.setAttribute("aria-selected", "false"))
     tipPercent = e.target.value
-    // console.log(billState, tipPercent, numberOfPeople)
+    updateState()
 })
 
 inputNumberOfPeople.addEventListener("input", (e)=> {
     numberOfPeople = e.target.value
-    // console.log(billState, tipPercent, numberOfPeople)
+    updateState()
 })
 
-// console.log(billState, tipPercent, numberOfPeople)
+
 
 
 
